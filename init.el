48bc5c7ab6c86d81a55808951fdf24593be02f3b
Mark Norton <remillard@gmail.com>
2022-07-05 09:38:34 -0500

    The unless doesn't seem to require package and need to make it explicit.

--- init.el
+++ init.el
@@ -12,19 +12,18 @@
 ;;
 ;; Setting up package archives
 ;;
-(unless package-enable-at-startup
-  (require 'package)
-  (setq package-archives
-        '(("gnu" . "https://elpa.gnu.org/packages/")
-          ("melpa" . "https://melpa.org/packages/")
-          ("org" . "https://orgmode.org/elpa/")))
-  (setq package-archive-priorities
-        '(("melpa" . 9)
-          ("org" . 10)
-          ("gnu" . 5)))
-  (package-initialize)
-  (unless package-archive-contents
-    (package-refresh-contents)))
+(require 'package)
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")
+        ("org" . "https://orgmode.org/elpa/")))
+(setq package-archive-priorities
+      '(("melpa" . 9)
+        ("org" . 10)
+        ("gnu" . 5)))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 ;;
 ;; Setup use-package
 ;;
